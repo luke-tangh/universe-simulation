9dfc478eb26e1e9d4437f0686a1801f47f414864
luketang <luke.tangh@outlook.com>
2023-11-06 15:52:51 +0000

    Random position generation

--- Main.hs
+++ Main.hs
@@ -1,21 +1,55 @@
 import Graphics.Gloss
 import System.Random
+import System.Random.Shuffle
+import System.IO.Unsafe
 
 mapWidth :: Int
-mapWidth = undefined
+mapWidth = 800
 
 mapHeight :: Int
-mapHeight = undefined
+mapHeight = 600
 
 stepsPerSec :: Int
 stepsPerSec = 30
 
+type Range t = (t, t)
+
+xPosRange :: Range Float
+xPosRange = (0,400)
+
+yPosRange :: Range Float
+yPosRange = (0,300)
+
+massRange :: Range Mass
+massRange = (0.1, 10)
+
+radiusRange :: Range Radius
+radiusRange = (1, 3)
+
 -- in kg/m^3
-densityRange :: (Density, Density)
+densityRange :: Range Density
 densityRange = (0.1, 500)
 
-massRange :: (Mass, Mass)
-massRange = (0.1, 10)
+shuffleList :: [a] -> [a]
+shuffleList xs = shuffle' xs (length xs) (mkStdGen (length xs)) 
+
+genXPos :: Int -> [Float]
+genXPos n = take n $ randomRs xPosRange (mkStdGen n)
+
+genYPos :: Int -> [Float]
+genYPos n = take n $ randomRs yPosRange (mkStdGen n)
+
+genXYPos :: Int -> [Point]
+genXYPos n = zip (shuffleList $ genXPos n) (genYPos n) 
+
+genMass :: Int -> [Float]
+genMass n = take n $ randomRs massRange (mkStdGen n)
+
+genRadius :: Int -> [Float]
+genRadius n = take n $ randomRs radiusRange (mkStdGen n)
+
+genDensity :: Int -> [Float]
+genDensity n = take n $ randomRs densityRange (mkStdGen n)
 
 type Mass = Float
 type Radius = Float
@@ -28,13 +62,23 @@ mapSetting :: Display
 mapSetting = InWindow "Window" (mapWidth, mapHeight) (0, 0)
 
 universe :: Universe
-universe = undefined
+universe = randomUniverse 35
 
+-- TODO: colour change 
 randomUniverse :: Int -> Universe
-randomUniverse = undefined
+randomUniverse atoms = 
+  consUniverse atoms (genXYPos atoms) (genMass atoms) (genRadius atoms) (genDensity atoms)
+
+consUniverse :: Int -> [Point] -> [Mass] -> [Radius] -> [Density] -> Universe
+consUniverse 0 _ _ _ _ = []
+consUniverse n (p:ps) (m:ms) (r:rs) (d:ds) = 
+  (p, (0,0), m, r, d, white) : consUniverse (n-1) ps ms rs ds 
 
 drawUniverse :: Universe -> Picture
-drawUniverse = undefined
+drawUniverse model
+  = Pictures
+  [translate xPos yPos $ color white $ circleSolid radius
+  | ((xPos, yPos), _, _, radius, _, c) <- model]
 
 updateUniverse :: vp -> Float -> Universe -> Universe
 updateUniverse = undefined
@@ -58,4 +102,5 @@ traceAtom :: Atom -> Picture
 traceAtom = undefined
 
 main :: IO ()
-main = simulate mapSetting black stepsPerSec universe drawUniverse updateUniverse
+main = display mapSetting black (drawUniverse universe)
+-- main = simulate mapSetting black stepsPerSec universe drawUniverse updateUniverse
--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Universe simulation in Haskell.
 ## Environment
 - `Ubuntu 22.04.3`
 - `gloss 1.13.2.2`
+
+## Run
+Install dependencies:
+```
+sudo apt-get install freeglut3 freeglut3-dev libgmp3-dev
+cabal install --lib gloss random random-shuffle
+```
+Generate output:
+```
+ghc -O2 -threaded -dynamic Main.hs
+```
